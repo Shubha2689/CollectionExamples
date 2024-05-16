8e958125e4ad5fcf0608f424c5b1ca145ac9d3cc
Shubhangi <shubhankurm89@gmail.com>
2024-05-15 19:10:11 -0500

    first commit

--- src/ListExample.java
+++ src/ListExample.java
@@ -1,2 +1,14 @@
+import java.util.LinkedList;
+
 public class ListExample {
+    public static void main(String[] args) {
+       {
+            LinkedList<String> cars = new LinkedList<String>();
+            cars.add("Volvo");
+            cars.add("BMW");
+            cars.add("Ford");
+            cars.add("Mazda");
+            System.out.println(cars);
+        }
+    }
 }
